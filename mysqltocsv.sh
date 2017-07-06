b877ca499931693e095700c63babb6e40c668ce5
Nic <nic@Ottoemezzo.simple.it>
2017-07-06 13:14:34 +0200

    add arguments config and conditions

--- mysqltocsv.sh
+++ mysqltocsv.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 # Export a MySQL database table into CSV format
 #
-# Usage: mysql-csv <database> <table> [fields] [extra parameters]
+# Usage: mysqltocsv <config> <database> <table> [fields] [header] [separator] [endline] [enclose] [escape] [endline];
 #
 # If fields is omitted '*' is assumed
 
-DATABASE="$1"
-TABLE="$2"
-FIELDS="$3"
-HEADER="$4"
-SEPARATOR="$5"
-ENDLINE="$6"
-shift; shift; shift
+CONFIG="$1"
+DATABASE="$2"
+TABLE="$3"
+FIELDS="$4"
+HEADER="$5"
+SEPARATOR="$6"
+ENCLOSE="$7"
+ESCAPE="$8"
+ENDLINE="$9"
+shift 9;
+
+if [ -z "$CONFIG" ]; then
+	echo "Usage: mysqltocsv <config> <database> <table> [fields] [header] [separator] [endline] [enclose] [escape] [endline]"
+	exit 1
+fi
 
 if [ -z "$DATABASE" ]; then
-	echo "Usage: mysql-csv <database> <table> [fields] [extra parameters]"
+	echo "Usage: mysqltocsv <config> <database> <table> [fields] [header] [separator] [endline] [enclose] [escape] [endline]"
 	exit 1
 fi
 
@@ -27,9 +35,33 @@ if [ -z "$FIELDS" ]; then
 	FIELDS="*"
 fi
 
+if [ -z "$SEPARATOR" ]; then
+	SEPARATOR=";"
+fi
+
+if [ -z "$ENDLINE" ]; then
+	ENDLINE="\r\n"
+fi
+
+if [ -z "$ENCLOSE" ]; then
+	ENCLOSE="\""
+fi
+
+if [ -z "$ESCAPE" ]; then
+	ESCAPE="\\\\"
+fi
+
 TMP=`mktemp -p /tmp -u mysql-csv.XXXXXX`
 
-mysql "$DATABASE" -e "SELECT $FIELDS INTO OUTFILE '$TMP' FIELDS TERMINATED BY ',' ENCLOSED BY '\"' ESCAPED BY '\\\\' LINES TERMINATED BY '\n' FROM $TABLE" "$@"
+QUERY="SELECT $FIELDS INTO OUTFILE '$TMP' FIELDS TERMINATED BY '$SEPARATOR' ENCLOSED BY '$ENCLOSE' ESCAPED BY '$ESCAPE' LINES TERMINATED BY '$ENDLINE' FROM $TABLE"
+
+if [ -n "$HEADER" ]; then
+    QUERY="(SELECT $HEADER) UNION (${QUERY})"
+fi
+
+mysql --login-path="$CONFIG" "$DATABASE" -e "$QUERY "$@"
 
 cat "$TMP"
 sudo rm "$TMP"
+
+exit 0
